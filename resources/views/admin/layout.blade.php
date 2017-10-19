d85c2470d589da772a8f78cbd8c43a7d8cc12e89
Cesar Gutierrez <cesargutierrezgt@gmail.com>
2017-10-19 12:20:47 -0600

    env

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -317,7 +317,7 @@ desired effect
             <h1 class="box-title">Sistema de Planillas</h1>
             <div class="box-tools pull-right">
               <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-              <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              {{-- <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> --}}
             </div>
           </div>
           <!-- /.box-header -->
