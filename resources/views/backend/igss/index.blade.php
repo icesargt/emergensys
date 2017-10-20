707b2a6f678465563eb90a5b4bdd0fc058911d8b
Cesar Gutierrez <cesargutierrezgt@gmail.com>
2017-10-19 22:07:00 -0600

    create routes

---
+++ resources/views/backend/igss/index.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layout')
+@section('content')
+
+<div class="container-fluid">
+	<div class="col-lg-12">
+		<table class="table table-bordered">
+  			texto aca
+		</table>
+	</div>
+</div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,33 @@ Route::get('dashboard', function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route, para grupo de rutas de Igss -> IgssController
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('igss-management', 'IggsController');
+});
+
+// Route, para grupo de rutas de Salario -> SalaryController
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('salary-management', 'SalaryController');
+});
+
+// Route, para grupo de rutas de Empleados -> EmployeeController
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('employee-management', 'EmployeeController');
+});
+
+// Route, para grupo de rutas Historial -> RecordController
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('record-management', 'RecordController');
+});
+
+// Route, para grupo de rutas de Planilla -> PayrollControler
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('payroll-management', 'PayrollControler');
+});
+
+// Route, para grupo de rutas de Detalles de planilla -> PayrollDetailController
+Route::group(['middleware' => ['auth'] ], function(){
+	Route::resource('payroll-details', 'PayrollDetailController');
+});
